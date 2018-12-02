d0b1630b4d2287568a1fc3644a3648e5e54e8bfa
Svetlana <florina33@gmail.com>
2018-12-02 03:09:23 +0200

    display formatted text from textarea

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
-import './styles/style.css';
+// import './styles/style.css';
 import $ from 'jquery';
+
+let messageField = $('.message-field');
+let messageFormatted = $('.message-formatted');
+
+function deleteVoid (val) {
+	return $.trim(val);
+}
+
+function formattedText (value) {
+	return deleteVoid(value).toLowerCase();
+}
+
+function displayValue (value) {
+	$(messageFormatted).text(value);
+}
+
+messageField.on('keyup input', function (event) {
+	// let valueTextarea = $(this).val();
+	let valueTextarea = event.target.value;
+
+	displayValue(formattedText(valueTextarea));
+});
