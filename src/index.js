8ed14afe4b02edf636cd8308cdaf507fdc37c4e7
Svetlana <florina33@gmail.com>
2018-12-10 15:44:37 +0200

    add regular expression to removeExtraSpaces () function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import './styles/style.css';
-import $ from 'jquery';
+// import './styles/style.css';
+// import $ from 'jquery';
 
 let messageField = $('.message-field');
 let messageFormatted = $('.message-formatted');
 
 function removeExtraSpaces (val) {
-	return $.trim(val);
+	// return $.trim(val);
+
+	//  \s   любой "пробельный" символ (по умолчанию - [ \t\n\r\f])
+	//   +   один или более раз ("жадный"), то же что {1,}
+	// Вы можете переключить все повторители в выражении в “не жадный” режим, воспользовавшись модификатором /g.
+	return val.replace(/\s+/g, ' ').trim();
 }
 
 function formattedText (value) {
@@ -16,7 +21,7 @@ function displayValue (value) {
 	messageFormatted.text(value);
 }
 
-messageField.on('keyup input', function (event) {
+messageField.on('input', function (event) {
 	let valueTextarea = event.target.value; //  $(this).val();
 
 	displayValue(formattedText(valueTextarea));
