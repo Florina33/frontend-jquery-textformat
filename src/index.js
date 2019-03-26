17fd3ef9b53ed2cf8a2b95aea82d5aa20e5f41fe
Svetlana <florina33@gmail.com>
2019-03-26 19:16:55 +0200

    reload project, fix main files

--- src/index.js
+++ src/index.js
@@ -1,51 +1,28 @@
-// import './styles/reset.css';
-// import './styles/style.css';
-// import './swiper-slider/swiper.min.css';
+import './styles/style.css';
+import $ from 'jquery';
 
-// import $ from 'jquery';
-// import './swiper-slider/swiper.min.js';
+let messageField = $('.message-field');
+let messageFormatted = $('.message-formatted');
 
-// ///////////////////////////////
+function removeExtraSpaces (val) {
+	// return $.trim(val);
 
-let swiper = new Swiper('.swiper-container', {
-	spaceBetween: 30,
-	centeredSlides: true,
-	autoplay: {
-		delay: 2500,
-		disableOnInteraction: false
-	}
+	//  \s   любой "пробельный" символ (по умолчанию - [ \t\n\r\f])
+	//   +   один или более раз ("жадный"), то же что {1,}
+	// Вы можете переключить все повторители в выражении в “не жадный” режим, воспользовавшись модификатором /g.
+	return val.replace(/\s+/g, ' ').trim();
+}
 
-	// pagination: {
-	// 	el: '.swiper-pagination',
-	// 	clickable: true
-	// },
-	// navigation: {
-	// 	nextEl: '.swiper-button-next',
-	// 	prevEl: '.swiper-button-prev'
-	// }
+function formattedText (value) {
+	return removeExtraSpaces(value).toLowerCase();
+}
+
+function displayValue (value) {
+	messageFormatted.text(value);
+}
+
+messageField.on('input', function (event) {
+	let valueTextarea = event.target.value; //  $(this).val();
+
+	displayValue(formattedText(valueTextarea));
 });
-
-
-// ///////////////////////////////
-
-
-// let messageField = $('.message-field');
-// let messageFormatted = $('.message-formatted');
-
-// function removeExtraSpaces (val) {
-// 	return val.replace(/\s+/g, ' ').trim();
-// }
-
-// function formattedText (value) {
-// 	return removeExtraSpaces(value).toLowerCase();
-// }
-
-// function displayValue (value) {
-// 	messageFormatted.text(value);
-// }
-
-// messageField.on('input', function (event) {
-// 	let valueTextarea = event.target.value; //  $(this).val();
-
-// 	displayValue(formattedText(valueTextarea));
-// });
